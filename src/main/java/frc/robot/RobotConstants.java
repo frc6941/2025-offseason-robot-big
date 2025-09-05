9c8ae9e6a114bcccc9b2a262429caf5a7b0f70e6
LCsiu <148622803+LouisCaiKM7@users.noreply.github.com>
2025-09-05 12:34:10 +0800

    Update RobotConstants.java

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -1,4 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
+f// Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
@@ -438,7 +438,7 @@ public final class RobotConstants {
     public static final TunableNumber INTAKE_PIVOT_JERK = new TunableNumber("INTAKE_PIVOT/jerk", 0);
     public static final TunableNumber INTAKE_PIVOT_TOLERANCE = new TunableNumber("INTAKE_PIVIOT/tolerance", 3.5);
 
-    public static final double INTAKE_PIVOT_ENCODER_OFFSET = -0.132568 - 0.25;
+    public static final double INTAKE_PIVOT_ENCODER_OFFSET = -0.160156-0.25;
     //Motion constants for intake roller
     public static final TunableNumber INTAKE_VOLTAGE = new TunableNumber("INTAKE_ROLLER/intakeVoltage", 15.0);
     public static final TunableNumber INDEX_ROLLER_VOLTAGE = new TunableNumber("INTAKE_ROLLER/indexRollerVoltage", 15.0);
